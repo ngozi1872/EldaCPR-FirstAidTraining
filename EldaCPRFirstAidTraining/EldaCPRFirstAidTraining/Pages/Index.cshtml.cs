da45f871b655e495995be5e0461e48181ee4176c
Kloteh Somah <klsomah@microsoft.com>
2020-10-14 23:44:13 -0400

    Kloteh checking e

--- EldaCPRFirstAidTraining/EldaCPRFirstAidTraining/Pages/Index.cshtml.cs
+++ EldaCPRFirstAidTraining/EldaCPRFirstAidTraining/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EldaCPRFirstAidTraining.Data;
+using EldaCPRFirstAidTraining.Utilities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using SQLitePCL;
 
@@ -14,14 +18,25 @@ namespace EldaCPRFirstAidTraining.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly EldaCPRFirstAidTrainingContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
         public DateTime UpComingTraining { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, EldaCPRFirstAidTrainingContext context)
+        [BindProperty]
+        public Student Student { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, EldaCPRFirstAidTrainingContext context, IWebHostEnvironment hostEnvironment)
         {
             _logger = logger;
             _context = context;
+            _hostingEnvironment = hostEnvironment;
+        }
 
+        public void OnGet()
+        {
             var training = _context.TrainingSchedules.OrderBy(t => t.StartDate).FirstOrDefault();
 
             if (training != null && training.StartDate != null)
@@ -29,11 +44,7 @@ namespace EldaCPRFirstAidTraining.Pages
             else
                 UpComingTraining = DateTime.Now.Date;
 
-        }
-
-        public void OnGet()
-        {
-
+            ViewData["TrainingId"] = new SelectList(GetSchedule(), "Id", "Title");
         }
 
         public IActionResult OnGetFindAllEvents()
@@ -48,5 +59,46 @@ namespace EldaCPRFirstAidTraining.Pages
             }).ToList();
             return new JsonResult(events);
         }
+
+        public IActionResult OnPostAddTraining()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Student.CreatedOn = DateTime.Now;
+
+            _context.Students.Add(Student);
+            _context.SaveChanges();
+
+
+            var logoPath = Path.Combine(_hostingEnvironment.WebRootPath, $"assets/images/NH-Logo.png");
+
+            //SendEmailFromGmail sfgmail = new SendEmailFromGmail();
+            //sfgmail.SendEmail(Student.Email, Student.FristName + " " + Student.LastName, "Elda CPR and First Aid Training Confirmation",
+            //        string.Format("Dear " + Student.FristName + ", <br/> Thanks for registering to for our " + myEvent.Name + " on " + date + " from " + myEvent.StartTime + " to " + myEvent.EndTime + ". <br/> We will follow up with you soon. <br/><br/> Nourishing Hands, Inc.<br/><br/>"), logoPath);
+
+            return RedirectToPage("./Index");
+        }
+
+        private IList<Schedule> GetSchedule()
+        {
+            IList<Schedule> sch = _context.TrainingSchedules.Select(e => new Schedule
+            {
+                Id = e.Id,
+                Title = $"{e.Training.Title}: {e.Training.Description } -${e.Training.Cost} on {e.StartDate.ToString("MM/dd/yy")}"
+            }).ToList();
+
+            return sch;
+        }
+
     }
+
+    public class Schedule 
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
 }
---
+++ EldaCPRFirstAidTraining/EldaCPRFirstAidTraining/Utilities/SendEmailFromGmail.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace EldaCPRFirstAidTraining.Utilities
+{
+    public class SendEmailFromGmail
+    {
+        public void SendEmail(string toEmail, string toName, string subject, string body, string path = null)
+        {
+            var fromAddress = new MailAddress("stringbuild1@gmail.com", "Nourishing Hands");
+            var toAddress = new MailAddress(toEmail, toName);
+            var copy = new MailAddress("stringbuild1@gmail.com");
+            const string fromPassword = "Trinity@2016";
+
+            var smtp = new SmtpClient
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+
+            };
+
+            string html = @"<html><body><img src='cid:NHLogo' style='display: block; margin-left: auto; margin-right: auto; width:75px; height:75px'><hr/><br/>" + body + "</body></html>";
+            AlternateView altView = AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html);
+
+            if (path != null)
+            {
+                LinkedResource yourPictureRes = new LinkedResource(path, MediaTypeNames.Image.Jpeg);
+                yourPictureRes.ContentId = "NHLogo";
+                altView.LinkedResources.Add(yourPictureRes);
+            }
+
+            var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            };
+            message.AlternateViews.Add(altView);
+            {
+                //await smtp.SendMailAsync(message);
+                message.CC.Add(copy);
+                message.IsBodyHtml = true;
+                smtp.Send(message);
+            }
+
+        }
+    }
+}
